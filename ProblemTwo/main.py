2d317136d0a840822f16451c5dbf0c40dc2bcb7d
Charbel Ghanem <ghanemcharbel219@gmail.com>
2026-03-19 13:32:10 +0200

    begin with problem two

---
+++ ProblemTwo/main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+
+df = pandas.read_csv('prediction.csv')
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    test_size=0.2,
+    random_state=42
+)
+
+
+model = RandomForestRegressor()
+
+model.fit(X_train, y_train)
